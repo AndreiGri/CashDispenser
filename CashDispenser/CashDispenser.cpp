e5e5b40a37a0bf968834119ae11a10ee02b93863
Andrei Gridin <andreigridin918@gmail.com>
2023-09-18 20:36:30 +0500

    Добавьте файлы проекта.

---
+++ CashDispenser/CashDispenser.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки терминала:
+    system("chcp 1251 > nul");
+
+    int limit = 100000;
+    int banknote = 100;
+    int summ = 0;
+
+    cout << endl;
+    cout << " Ввдите сумму для снятия: ";
+    cin >> summ;
+
+    if (summ > limit) {
+        cout << " Банкомат не может выдать " << summ << " рублей т.к. данная сумма превышает лимит в " << limit << " рублей!" << endl;
+    }
+    else {
+        if (summ % banknote != 0) {
+            cout << " Банкомат не может выдать " << summ << " рублей т.к. данная сумма не кратна " << banknote << " рублям!" << endl;
+        }
+        else {
+            if (summ <= 0) {
+                cout << " Вы ввели отрицательную сумму!" << endl;
+            }
+            else {
+                cout << " Не забудте забрать ваши " << summ << " рублей!" << endl;
+            }
+        }
+    }
+
+    // Задержка окна консоли:
+    system("pause > nul");
+
+    return 0;
+}
